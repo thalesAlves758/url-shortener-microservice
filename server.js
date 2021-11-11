8ef688b9de287f76ac27e00024e7ef03997e0192
unknown <thalesalves758@gmail.com>
2021-11-11 03:06:48 -0300

    Remove an unnecessary route

--- server.js
+++ server.js
@@ -18,11 +18,6 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
 app.post('/api/shorturl', (req, res) => {
 
   const { url } = req.body;
